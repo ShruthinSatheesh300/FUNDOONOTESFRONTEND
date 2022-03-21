8571c32e28fd8d8cebb5146d9d1e66bf3634c787
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-03-21 22:18:01 +0530

    Added essential Components Imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'registration',component:RegistrationComponent},
+  {path:'signin',component:SigninComponent},
+  {path:'forgetPassword',component:ForgetPasswordComponent},
+  {path: 'resetPassword',component:ResetPasswordComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
