ec0a67d6f22af07087c0fb609e86331a5b9d964c
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-03-24 17:01:42 +0530

    Added routs for dashboard and takenote

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { SigninComponent } from './components/signin/signin.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TakenoteComponent } from './components/takenote/takenote.component';
 
 const routes: Routes = [
   {path:'registration',component:RegistrationComponent},
   {path:'signin',component:SigninComponent},
   {path:'forgetPassword',component:ForgetPasswordComponent},
   {path: 'resetPassword/:token',component:ResetPasswordComponent},
-  {path: 'dashboard',component:DashboardComponent}
+  {path: 'dashboard',component:DashboardComponent},
+  {path: 'dashboard/takenote',component:TakenoteComponent}
 
 ];
 
