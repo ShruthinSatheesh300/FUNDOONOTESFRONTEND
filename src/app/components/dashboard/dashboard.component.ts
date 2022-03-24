61b4bac27908dbcfedf021080b5647eb4da63345
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-03-24 17:04:50 +0530

    Added essential methods in ts file of dashboard

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  value = '';
+  toggle = true;
+  status = "Enable";
+  
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  logOut() {
+    this.router.navigateByUrl('/signin')
+  }
+  enableDisableRule() {
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? "Enable" : "Disable";
+  }
+  onnote(){
+    this.router.navigateByUrl('/dashboard/takenote')
+  }
+
+ 
+}
