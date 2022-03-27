4e737a3f35252b9584987df2a5b46895c1962952
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-03-27 22:30:52 +0530

    Added component for Take note

--- src/app/components/takenote/takenote.component.ts
+++ src/app/components/takenote/takenote.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NoteService } from 'src/app/Services/note.service';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-takenote',
   templateUrl: './takenote.component.html',
@@ -7,25 +8,40 @@ import { NoteService } from 'src/app/Services/note.service';
 })
 export class TakenoteComponent implements OnInit {
   panelOpenState = false;
-  Title:any
+  div1: boolean = true;
+  div2: boolean = false;
+  span1: boolean = false;
+  Title: any;
   Description: any;
-  
-  
 
-  constructor(private NoteService:NoteService) { }
 
+  constructor(private NoteService: NoteService ) { }
   ngOnInit(): void {
-
+    
   }
-  onClose(){
+  expansiondiv() {
+    this.div1 = false;
+    this.span1 = true;
+    this.div2 = true;
+  }
+  closeexpansiondiv() {
+    this.div1 = true;
+    this.span1 = false;
+  }
+  
+  onclose() {
+    this.span1 = false;
+    this.div1 = true;
+    this.panelOpenState = false;
+    this.div2 = false;
+
     let reqData = {
-     Title : this.Title,
-     Description:this.Description
+      'Title': this.Title, 
+      'Description': this.Description
     }
     
     this.NoteService.takeNoteService(reqData).subscribe((res: any) => {
       console.log(res);
     })
   }
-
 }
