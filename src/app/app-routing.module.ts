89586f7ea1fdd3de0390f7548ddd7dce66687abc
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-03-27 22:15:47 +0530

    Added GetallNotes Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,24 @@ import { SigninComponent } from './components/signin/signin.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { TakenoteComponent } from './components/takenote/takenote.component';
+import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
 
 const routes: Routes = [
   {path:'registration',component:RegistrationComponent},
   {path:'signin',component:SigninComponent},
   {path:'forgetPassword',component:ForgetPasswordComponent},
   {path: 'resetPassword/:token',component:ResetPasswordComponent},
-  {path: 'dashboard',component:DashboardComponent},
-  {path: 'dashboard/takenote',component:TakenoteComponent}
+  // {path: 'dashboard',component:DashboardComponent},
+
+  {
+    path: 'dashboard',component:DashboardComponent,
+    children: [
+      {path:'', redirectTo:"/dashboard/getallnotes", pathMatch:'full' },
+      // { path: 'takenote', component: TakenoteComponent },
+       { path: 'getallnotes', component: GetAllNotesComponent },
+    ]
+  
+  }
 
 ];
 
