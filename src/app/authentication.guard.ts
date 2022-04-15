b12408214e7548dc7fc0f79d8c08bd293038e4e1
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-04-15 19:40:23 +0530

    Added authService and url to navigate to signin

--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthGuardServiceService } from './auth-guard-service.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
-  canActivate () : boolean
-  {
-    return true;
-  }
-  
+   constructor(private Authguardservice: AuthGuardServiceService, private router: Router){}
+  canActivate () : boolean{
+  if (!this.Authguardservice.gettoken()) {  
+    this.router.navigateByUrl("/signin");  
+}  
+return this.Authguardservice.gettoken();  
+}  
 }
+  
+
