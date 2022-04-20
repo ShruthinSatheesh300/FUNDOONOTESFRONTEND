1127adc92f27637b0cd7bdf3e1f68ea69be4cdfb
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-04-20 08:40:28 +0530

    Added data service for unrelated sharing

---
+++ src/app/Services/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+  private messageSource = new BehaviorSubject([]);
+  currentMessage = this.messageSource.asObservable();
+  changeMessage(message: any) {
+    this.messageSource.next(message)
+  }
+  private searchData = new BehaviorSubject({type:'',data:[]});
+  searchNote = this.searchData.asObservable();
+  changeData(message: any) {
+    this.searchData.next(message)
+  }
+}
+
